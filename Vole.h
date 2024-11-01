204849e054aea4a0bc211fd9e593d80dd19232c8
amirsloker2005 <162839071+amirsloker2005@users.noreply.github.com>
2024-11-01 21:35:43 +0300

    Update Vole.h

--- Vole.h
+++ Vole.h
@@ -38,14 +38,32 @@ public:
 
 
 class ALU{
-
+private:
+    unordered_map<char, string> X4B = {
+            {'0', "0000"}, {'1', "0001"}, {'2', "0010"}, {'3', "0011"},
+            {'4', "0100"}, {'5', "0101"}, {'6', "0110"}, {'7', "0111"},
+            {'8', "1000"}, {'9', "1001"}, {'A', "1010"}, {'B', "1011"},
+            {'C', "1100"}, {'D', "1101"}, {'E', "1110"}, {'F', "1111"}
+    };
+    unordered_map<string, string> FourBX = {
+            {"0000", "0"}, {"0001", "1"}, {"0010", "2"}, {"0011", "3"},
+            {"0100", "4"}, {"0101", "5"}, {"0110", "6"}, {"0111", "7"},
+            {"1000", "8"}, {"1001", "9"}, {"1010", "A"}, {"1011", "B"},
+            {"1100", "C"}, {"1101", "D"}, {"1110", "E"}, {"1111", "F"}
+    };
+    unordered_map<int, string> X3B = {
+            {0, "000"}, {1, "001"}, {2, "010"}, {3, "011"},
+            {4, "100"}, {5, "101"}, {6, "110"}, {7, "111"},
+    };
+    string floattohex(double floating);
+    double floatextract(string hexa);
 public:
 
     string hexToDecimal(string hexa_num);
     string decimalToHex(string decimal_num);
     bool isValid(string statment);
-    void add(string idx1, string idx2, Register &register1);
-
+    void add(string idx1, string idx2, Register &register1, string address);
+    void addF(string idx1, string idx2, Register &register1, string address);
 };
 
 class CU{
